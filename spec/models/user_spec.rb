a0b71d9bfc9acce40a3683646179f6e97c835e22
Romi Elan <romielan@gmail.com>
2013-06-11 18:57:13 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,15 +25,21 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
   
-  	describe "return value of authenticate method" do
+  	describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
+
+    describe "return value of authenticate method" do
   		before { @user.save }
   		let(:found_user) { User.find_by_email(@user.email) }
   	
   	describe "with valid password" do
     	it { should == found_user.authenticate(@user.password) }
-  	end
+    end
   	
   	describe "with invalid password" do
     	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
